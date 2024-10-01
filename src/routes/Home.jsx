b3c0b989eee1644fb961d8d94c78c8c3f02d7f07
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2024-10-01 20:28:11 +0530

    Minor Changes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
     <div>
         <h1 className=" text-5xl font-bold bg-gradient-to-r from-zinc-600 via-slate-500 to-green-400 text-transparent bg-clip-text text-center p-8">Welcome to Halo Waypoint</h1>
         <form onSubmit={(e)=>handleAdd(e)} className='justify-center'>
-            <input classname="flex items-center" type='email' name='title' placeholder='email@example.com'/> <br/>
+            <input classname="flex items-center" type='email' name='title' placeholder='email@example.com' required/> <br/>
             <button className="bg-sky-500 hover:bg-sky-700 rounded-full py-6 px-8 text-white transition ease-in-out delay-150 hover:-translate-y-1">Submit</button>
         </form>
 
